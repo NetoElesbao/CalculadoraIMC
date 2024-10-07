5ba34fc3106b980b570023905c1c5bb296e43423
Neto Elesbão <josenetu32@gmail.com>
2024-10-07 17:05:29 -0300

    V1.0

---
+++ src/components/ImcCalc.jsx
@@ -0,0 +1,38 @@
+import Button from "./Button";
+import "./ImcCalc.css";
+
+const ImcCalc = () => {
+  return (
+    <div id="calc-imc">
+      <h2>Calculadora IMC</h2>
+      <form action="" id="imc-form">
+        <div className="form-inputs">
+          <div className="form-control">
+            <label htmlFor="height">Altura:</label>
+            <input
+              type="text"
+              name="height"
+              id="height"
+              placeholder="Exemplo: 1,75"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="weight">Peso:</label>
+            <input
+              type="text"
+              name="weight"
+              id="weight"
+              placeholder="Exemplo: 70,5"
+            />
+          </div>
+        </div>
+        <div className="action-control">
+          <Button id="calc-btn" text="Calcular" />
+          <Button id="clear-btn" text="Limpar" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ImcCalc;

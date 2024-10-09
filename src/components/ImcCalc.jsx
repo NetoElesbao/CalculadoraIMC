7e45ac78a04f218a565ee93180a74a030bfd1f18
Neto Elesbão <josenetu32@gmail.com>
2024-10-09 17:33:05 -0300

    V1.1

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import Button from "./Button";
 import "./ImcCalc.css";
 
 const ImcCalc = () => {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const ClearForm = (event) => {
+    event.preventDefault();
+    setHeight("");
+    setWeight("");
+  };
+
   return (
     <div id="calc-imc">
       <h2>Calculadora IMC</h2>
@@ -14,6 +24,8 @@ const ImcCalc = () => {
               name="height"
               id="height"
               placeholder="Exemplo: 1,75"
+              onChange={(event) => setHeight(event.target.value)}
+              value={height}
             />
           </div>
           <div className="form-control">
@@ -23,12 +35,14 @@ const ImcCalc = () => {
               name="weight"
               id="weight"
               placeholder="Exemplo: 70,5"
+              onChange={(event) => setWeight(event.target.value)}
+              value={weight}
             />
           </div>
         </div>
         <div className="action-control">
           <Button id="calc-btn" text="Calcular" />
-          <Button id="clear-btn" text="Limpar" />
+          <Button id="clear-btn" text="Limpar" action={ClearForm} />
         </div>
       </form>
     </div>
